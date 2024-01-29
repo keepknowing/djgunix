344dce135f6991262b5a78e79afe0a47859683b6
keepknowing <eduardojucaalmeida@gmail.com>
2024-01-29 15:32:29 -0300

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date  = models.DateField()
+
+    def __str__(self) -> str:
+        return self.title
+    
